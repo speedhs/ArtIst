d87201b73ab2b10e8adec88b76392fb04540beb1
speedhs <cocattackbased@gmail.com>
2023-04-03 22:35:50 +0530

    Home page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import {logo} from './assets';
 import {Home, CreatePost} from './pages';
 
-const App = () => {
-  return (
+const App = () => (
+
     <BrowserRouter>
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to = "/">
@@ -16,14 +16,12 @@ const App = () => {
         </Link>
       </header>
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh - 73px)]'>
-        <Routes>
-          <Route path='/' element = {<Home/>}/>
-          <Route path='/create-post' element = {<CreatePost/>}/>
-          
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/create-post" element={CreatePost} />
+      </Routes>
       </main>
     </BrowserRouter>
-    )
-}
+    );
 
 export default App
